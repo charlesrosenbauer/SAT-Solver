1ffe73d2febc4cf519fa52fa07489aba10bb5655
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-12-20 13:09:04 +0000

    Added function to count mentions of variables in clauses

--- src/solver.c
+++ src/solver.c
@@ -1,2 +1,29 @@
 #include "alloc.h"
 #include "solver.h"
+#include "stdlib.h"
+
+
+
+
+
+
+
+
+
+
+int* countMentions(CNF* cnf){
+  int* mentions = malloc(sizeof(int) * cnf->varnum);
+
+  for(int i = 0; i < cnf->varnum; i++)
+    mentions[i] = 0;
+
+  for(int i = 0; i < cnf->clausenum; i++){
+    Clause c = cnf->clauses[i];
+
+    for(int j = 0; j < c.numvars; j++)
+      mentions[c.vars[j]]++;
+
+  }
+
+  return mentions;
+}
