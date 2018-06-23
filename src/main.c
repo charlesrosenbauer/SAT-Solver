fad694c4b63283f3ef4a96df28d99fb4f3b64e6c
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-06-23 18:28:57 +0000

    Removed code related to persistent byte array. I have a much better idea

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "cnfparser.h"
 #include "stdio.h"
 #include "stdlib.h"
-#include "alloc.h"
 #include "global.h"
 
 
@@ -52,33 +51,6 @@ int main(int argc, char** argv){
 
   IntPair* mentions = sortByMentions(&cnf);
 
-  #ifdef _TEST_MODE_
-  int limit = 16384;
-  PersistentByteArray* arr = createByteArray(limit);
-  printf("Depth: %i\n", arr->depth);
-
-  for(int i = 0; i < limit; i += 64){
-    printf("%i %p \n", i, pbaPointer(arr, i));
-    //printf("%i\n", (int)pbaRead   (arr, i));
-  }
-  #endif
-
-  /*
-  // I'll worry about the allocator later
-  Allocator* allocator = mkAllocator(16384);
-  pushFrame(allocator);
-  for(int i = 0; i < 1048576; i++){
-    int* x = (int*)alloc(allocator, 400);
-    *x = i;
-    if(i % 4096 == 0) printf("!!\n");
-    if(i % 65536 == 0){
-      printf("---\n");
-
-      popFrame(allocator);
-      pushFrame(allocator);
-    }
-  }
-  */
 
   return 0;
 }
