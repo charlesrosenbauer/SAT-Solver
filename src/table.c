3d15480bb631f46969ed020664d7aeda5a628df0
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-08-19 23:28:46 +0000

    Stripped out some table code, we're going to do this a bit differently.

--- src/table.c
+++ src/table.c
@@ -1,6 +1,7 @@
 #include "table.h"
 #include "stdint.h"
 #include "stdlib.h"
+#include "util.h"
 
 
 
@@ -64,71 +65,7 @@ TABLE* initTable(CNF* c, int64_t sizeSuggest){
 
   TABLE* ret = malloc(sizeof(TABLE));
 
-  /*
-    Around here we should probably sort clauses and parameters to get them into
-    a friendly order, but we'll save that for later.
-  */
 
-  if(sizeSuggest <= 0){
-    sizeSuggest = c->clausenum * 2;
-  }
-
-  ret->allCells = malloc(sizeof(TABLECELL) * sizeSuggest);
-
-  ret->cols = (c->varnum % 256)? (c->varnum / 256)+1 : c->varnum;
-  ret->rows =  c->clausenum;
-
-
-  // Generate Table Cells
-
-  uint64_t cellIndex = 0;
-
-  TABLECELL* columnPad = malloc(sizeof(TABLECELL) * c->clausenum);
-  int stackTop = 0;
-
-  for(int i = 0; i < ret->cols; i++){   // Parameters
-    for(int j = 0; j < ret->rows; j++){   // Clauses
-
-      /*
-        Rows are the inner loop here because most operations on this table will
-        iterate over data from within the same column. As a result,
-      */
-
-      Clause* cl = &(c->clauses[j]);
-
-      TABLECELL cell = initCell(i, j);
-
-      int isCellModified = 0;
-
-      for(int k = 0; k < cl->numvars; k++){
-        int x = cl->vars[k];
-
-        if((x >= (i * 256)) && (x < ((i+1) * 256))){
-          // Fill Cell if possible
-          IX xi = x / 256;
-          IX xj = x % 256;
-          cell.vals[xi] = (1 << xj);
-          cell.mask[xi] = (1 << xj);
-          isCellModified = 1;
-        }
-      }
-      columnPad[stackTop] = cell;
-      stackTop++;
-    }
-
-    // TODO: Sort columnPad by bit patterns, generate column header.
-
-    COLUMNHEADER* header = malloc(sizeof(COLUMNHEADER));
-
-    // TODO: Transfer column to Table contents.
-
-  }
-
-  ret->cellCount = cellIndex + 1;
-
-
-
-  free(columnPad);
 
   return ret;
 }
