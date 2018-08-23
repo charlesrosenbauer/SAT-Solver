20bba7a6143eafb636149f0d80ef2eaa4e08c3a7
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-08-23 23:52:54 +0000

    Additions to the initTable function

--- src/table.c
+++ src/table.c
@@ -13,6 +13,7 @@
 
 
 void freeTable(TABLE* t){
+  /*
   for(int i = 0; i < t->cols; i++)
     free(t->paramIndex[i]);
   free(t->paramIndex);
@@ -26,6 +27,7 @@ void freeTable(TABLE* t){
   free(t->allCells);
 
   free(t);
+  */
 }
 
 
@@ -65,6 +67,49 @@ TABLE* initTable(CNF* c, int64_t sizeSuggest){
 
   TABLE* ret = malloc(sizeof(TABLE));
 
+  //Get the total number of table cells.
+  int cellct = 0;
+  for(int i = 0; i < c->clausenum; i++){
+    Clause cl = c->clauses[i];
+
+    /*
+      We need to count how many cells there are per column. The problem is that
+      we need a set to do this properly, and sets can be pretty slow.
+
+      A small bloom filter should be able to handle most cases. An N^2 lookup
+      could be done in theory, but that might be a bit slow. So let's just check
+      a bloom filter and a "last" value. If the last check fails and the hash
+      passes, then we can do a worst-case check.
+
+      I'm assuming CNF vars aren't sorted. If they are, then the whole hash and
+      worst-case check is redundant and the "last" value is good enough.
+
+      This should be able to handle most cases pretty efficiently.
+    */
+    uint64_t hash =  0;
+    uint64_t last = -1;
+
+    for(int j = 0; j < cl.numvars; j++){
+      int x = cl.vars[j] / 256;
+      uint64_t hashBit = (1 << (x % 64));
+      if(x != last){
+        last = x;
+
+        // Check hash
+        if(hashBit & hash){
+          int matches = 0;
+          // Worst-case check
+          for(int k = 0; k < j; k++)
+            matches += (cl.vars[k] == cl.vars[j]);
+
+          cellct += matches != 0;  // Increment pass only if no matches
+        }else{
+          cellct++;
+          hash |= hashBit;
+        }
+      }
+    }
+  }
 
 
   return ret;
